5e28a5aeb1af0db2d3966279cb62abb6b2f40e07
Paola <105376831+PaolaUniSa@users.noreply.github.com>
2024-05-16 23:32:20 +0200

    Update RecurrentSpikingNeuron.v

--- src/RecurrentSpikingNeuron.v
+++ src/RecurrentSpikingNeuron.v
@@ -1,5 +1,3 @@
-`timescale 1ns / 1ps
-
 module RecurrentSpikingNeuron(
     input wire clk,                          // Clock signal
     input wire reset,                        // Asynchronous reset, active high
